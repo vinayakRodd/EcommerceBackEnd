05339f28bb7d6c53cf35e1eea063c1799657db00
Vinayak Rodd <150567680+vinayakRodd@users.noreply.github.com>
2024-12-03 23:10:27 +0530

    Add files via upload

--- BackEnd/MongoConnect.js
+++ BackEnd/MongoConnect.js
@@ -1,7 +1,7 @@
 const mongodb=require('mongodb')
 
-const connectionUrl="mongodb://127.0.0.1:27017/ShoppingCart" // 0.0.0.0:27017/name_of_collection //
-
+// const connectionUrl="mongodb://127.0.0.1:27017/ShoppingCart" // 0.0.0.0:27017/name_of_collection //
+const connectionUrl = "mongodb+srv://roddvinayak709:k4DCXFzgTfGvAk6Q@cluster0.tlhg0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const client=new mongodb.MongoClient(connectionUrl)
 
 var db;
